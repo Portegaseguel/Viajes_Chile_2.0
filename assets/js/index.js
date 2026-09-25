b29f9cb3fbbb5c3125236cab53adc98b9d27850d
Paulina <frontend.portega@gmail.com>
2026-09-25 11:39:00 -0300

    Fix frontend script errors

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,36 +1,28 @@
-window.addEventListener('scroll', function() {
-  const navbar = document.getElementById('navbar');
-  if (window.scrollY > 50) {
-      navbar.style.backgroundColor = 'rgba(0, 0, 0, 1)';  
-  } else {
-      navbar.style.backgroundColor = 'transparent';  
-  }
+window.addEventListener('scroll', function () {
+    const navbar = document.getElementById('navbar');
+
+    if (window.scrollY > 50) {
+        navbar.style.backgroundColor = 'rgba(0, 0, 0, 1)';
+    } else {
+        navbar.style.backgroundColor = 'transparent';
+    }
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+    const tooltipTriggerList = [].slice.call(
+        document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    );
 
-document.addEventListener('DOMContentLoaded', function() {
-   let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-   
-   let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-       return new bootstrap.Tooltip(tooltipTriggerEl);
-   });
- });
-
-document.getElementById('myForm').addEventListener('submit', function(event) {
-event.preventDefault();
-
-alert('Formulario enviado con éxito!');
+    tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
 });
 
-let myModal = document.getElementById('myModal')
-let myInput = document.getElementById('myInput')
+document.getElementById('myForm').addEventListener('submit', function (event) {
+    event.preventDefault();
 
-myModal.addEventListener('shown.bs.modal', function () {
-myInput.focus()
-})
-
-
-
+    alert('Formulario enviado con éxito!');
+});
 
 
 
