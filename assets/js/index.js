0401e96d28cdd8254cdaf04dd794365449198cca
Paulina <frontend.portega@gmail.com>
2026-09-25 11:58:59 -0300

    Restore original index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,31 +1,33 @@
-window.addEventListener('scroll', function () {
-    const navbar = document.getElementById('navbar');
-
-    if (window.scrollY > 50) {
-        navbar.style.backgroundColor = 'rgba(0, 0, 0, 1)';
-    } else {
-        navbar.style.backgroundColor = 'transparent';
-    }
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-    const tooltipTriggerList = [].slice.call(
-        document.querySelectorAll('[data-bs-toggle="tooltip"]')
-    );
-
-    tooltipTriggerList.map(function (tooltipTriggerEl) {
-        return new bootstrap.Tooltip(tooltipTriggerEl);
-    });
-});
-
-document.getElementById('myForm').addEventListener('submit', function (event) {
-    event.preventDefault();
-
-    alert('Formulario enviado con éxito!');
+window.addEventListener('scroll', function() {
+  const navbar = document.getElementById('navbar');
+  if (window.scrollY > 50) {
+      navbar.style.backgroundColor = 'rgba(0, 0, 0, 1)';  
+  } else {
+      navbar.style.backgroundColor = 'transparent';  
+  }
 });
 
 
+document.addEventListener('DOMContentLoaded', function() {
+   let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+   
+   let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+       return new bootstrap.Tooltip(tooltipTriggerEl);
+   });
+ });
 
+document.getElementById('myForm').addEventListener('submit', function(event) {
+event.preventDefault();
+
+alert('Formulario enviado con éxito!');
+});
+
+let myModal = document.getElementById('myModal')
+let myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+myInput.focus()
+})
 
 
 
